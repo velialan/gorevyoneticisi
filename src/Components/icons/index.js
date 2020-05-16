74f0c6dbec1d210e9d26369d4f9693d2ab4c5c14
velialan <veli.alan03@gmail.com>
2020-05-16 20:36:59 +0300

    iconlar dışarı aktarıldı

--- src/Components/icons/index.js
+++ src/Components/icons/index.js
@@ -1,4 +1,7 @@
+export { default as Bell } from './Bell'
+export { default as Check } from './Check'
 export { default as Grid } from './Grid'
 export { default as Home } from './Home'
 export { default as Plus } from './Plus'
 export { default as Trash2 } from './Trash2'
+export { default as X } from './X'
