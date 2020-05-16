1c48aceff923e042724e050eef6484f29aebbbdb
velialan <veli.alan03@gmail.com>
2020-05-16 20:34:16 +0300

    styled system border radius eklendi

--- src/Components/box.js
+++ src/Components/box.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
 import {View} from 'react-native';
-import {compose, color, size, flexbox,space } from 'styled-system'
+import {compose, color, size, flexbox,space,borderRadius,zIndex} from 'styled-system'
 
 const Box = styled(View)(
     compose(
         space,
         color,
         size,
-        flexbox
+        flexbox,
+        borderRadius,
+        zIndex
     ),
 );
  
