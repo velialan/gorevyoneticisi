e259cdcde2b941a909f1a3731404138687d64d0b
velialan <veli.alan03@gmail.com>
2020-05-20 04:33:53 +0300

    açıklama satırları eklendi

--- src/Components/box.js
+++ src/Components/box.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
-import {View} from 'react-native';
-import {compose, color, size, flexbox,space,borderRadius,zIndex} from 'styled-system'
-
+import { View } from 'react-native';
+import { compose, color, size, flexbox, space, borderRadius, zIndex } from 'styled-system'
+//color renk seçimleri için
+//size gelen değer view'e width height olarak işleniyor
+//flexbox flex değerleri için,
+//space margin padding bottom top vs değerleri almak için
+//z index  çok kullanmadım ama kullanırım diye en çok kullandıklarımı ekliyorum.
 const Box = styled(View)(
     compose(
         space,
@@ -12,6 +16,10 @@ const Box = styled(View)(
         zIndex
     ),
 );
- 
+//default proplar ekleyebiliyoruz örneğin tüm boxlarda borderRadius değeri 5 yapmak gibi
+// Box.defaultProps = {
+//     p: 2,
+//     bg: 'white',
+//   }
 
 export default Box
