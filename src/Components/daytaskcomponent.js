255186e1129482a3312b4f186a506b88f77596fc
velialan <veli.alan03@gmail.com>
2020-05-20 04:35:48 +0300

    açıklamalar ve bugün ve yarının datalarını çektik

--- src/Components/daytaskcomponent.js
+++ src/Components/daytaskcomponent.js
@@ -1,24 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { CardTitle, CardContainer, CardCheck, CardDate, CardAlarm } from './card';
-import { SectionList } from 'react-native';
+import { SectionList, Alert } from 'react-native';
 import Text from '../Components/text';
 import Data from '../sqlite/data'//sqlite işlemlerini kolaylaştıran kütüphane
 var service = new Data();
 
 
 function DayTaskComponent() {
-    const [tasklist, settasklist] = useState([]);
-    
-    useEffect(() => {
-        refresh()
-    })
+    const [tasklisttoday, settasklisttoday] = useState([]);
+    const [tasklisttomorrow, settasklisttomorrow] = useState([]);
 
-    const refresh = async () => {
-        var result = await service.select("gorevler")
-        settasklist(result)
+    useEffect( () => {
+        today()
+        tomorrow()
+       // alert(gettomorrowDate())
+    })
+    const getcurrentDate = (gunarttir=0) => {
+
+        var zeromonth = '';// başına sıfır eklemek için (günlerin)
+        var date = new Date().getDate()+gunarttir;//10 dan küçükse başında 0 gelmiyor
+        var month = new Date().getMonth() + 1;
+        var year = new Date().getFullYear();
+        if (month < 10) {
+            zeromonth = '0' + month;
+        } else {
+            zeromonth = month;
+        }
+        return year + '-' + zeromonth + '-' + date;
+
     }
-    const completedtask = (id) => {
-        //sqlite üzerinde update işlemi tamamlanan görevler       
+
+   
+    //bugün yapılacaklar listesi
+    const today = async () => {
+        var result = await service.select("gorevler","*",{date:getcurrentDate(0)})
+        settasklisttoday(result)
+    }
+    //yarın yapılacaklar litesi
+    const tomorrow = async () => {
+        var result = await service.select("gorevler","*",{date:getcurrentDate(1)})
+        settasklisttomorrow(result)
+    }
+
+
+    const completedtask = async (id) => {
+      await  service.update("gorevler",{completed:true},{id:id})
+        alert('görev tamamlandı')
+        today();
+        tomorrow();
     }
     const createalarm = (id) => {
         //sqlite üzerinde update işlemi alarm ekleme      
@@ -26,14 +55,14 @@ function DayTaskComponent() {
     return (
         <SectionList
             sections={[
-                { title: 'Bugün', data: tasklist },
-                { title: 'Yarın', data: tasklist },
+                { title: 'Bugün', data: tasklisttoday },
+                { title: 'Yarın', data: tasklisttomorrow },
             ]}
             renderSectionHeader={({ section }) => (
-                <Text px={8} py={10} fontSize={18} fontWeight="bold" color="#8B87B3">{section.title}</Text>
+                <Text px={15} py={10} fontSize={18} fontWeight="bold" color="#8B87B3">{section.title}</Text>
             )}
             renderItem={({ item, index }) =>
-                <CardContainer  index={index} mx={5} my={5} flexDirection='row' alignItems='center'>
+                <CardContainer index={index} mx={5} my={5} flexDirection='row' alignItems='center'>
                     <CardCheck onPress={() => completedtask(item.id)} bg={item.completed ? 'checkcontainercolor' : 'white'} finish={item.completed} />
                     <CardDate px={5}>{item.time}</CardDate>
                     <CardTitle>{item.title}</CardTitle>
