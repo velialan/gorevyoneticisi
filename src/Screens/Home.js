5c65e654cbdb69b9f752c16d3ffb9de3b906aaec
velialan <veli.alan03@gmail.com>
2020-05-21 18:02:23 +0300

    performans iyileştirmeleri

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StatusBar, SectionList } from 'react-native';
 import { HeeaderContainer, HeaderInfo } from '../Components/header';//header componentimiz
 import Box from '../Components/box';//box (View) styled system components import ettik
 import { HeeaderReminder } from '../Components/headerreminder'; //hatırlatıcıyı import ediyoruz
-import Data from '../sqlite/data'//sqlite da CRUD işlemlerini kolaylaştıran kütübhane kod kalabalığından kurtarır
-import DayTaskComponent from '../Components/daytaskcomponent';//componentlerimizi impor ediyoruz
+import { CardTitle, CardContainer, CardCheck, CardDate, CardAlarm } from '../Components/card';
+import Text from '../Components/text';
 var service = new Data();
-
+import Data from '../sqlite/data'//sqlite da CRUD işlemlerini kolaylaştıran kütübhane kod kalabalığından kurtarır
 
 function Home() {
   service.init();//veri tabanımız oluşturuldu
-   useEffect(() => {
+
+  useEffect(() => {
 
     //görevler tablosu oluşturuldu
     service.createTable("gorevler", [
@@ -103,7 +104,54 @@ function Home() {
     // })
 
   }, []);
+  const [tasklisttoday, settasklisttoday] = useState([]);
+  const [tasklisttomorrow, settasklisttomorrow] = useState([]);
 
+  useEffect(() => {
+    today()
+    tomorrow()
+    // alert(gettomorrowDate())
+  })
+
+  const getcurrentDate = (deger = null) => {
+
+    var zeromonth = '';// başına sıfır eklemek için (günlerin)
+    var date = new Date().getDate() + deger;//10 dan küçükse başında 0 gelmiyor
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    if (month < 10) {
+      zeromonth = '0' + month;
+    } else {
+      zeromonth = month;
+    }
+    return year + '-' + zeromonth + '-' + date;
+  }
+
+
+  //bugün yapılacaklar listesi
+  const today = async () => {
+    var result = await service.select("gorevler", "", { date: getcurrentDate(0) })
+    settasklisttoday(result)
+  }
+  //yarın yapılacaklar litesi
+  const tomorrow = async () => {
+    var result = await service.select("gorevler", "", { date: getcurrentDate(1) })
+    settasklisttomorrow(result)
+  }
+
+
+  const completedtask = async (id) => {
+    await service.update("gorevler", { completed: true }, { id: id })
+    alert('görev tamamlandı')
+    today();
+    tomorrow();
+  }
+  const createalarm = async(id) => {
+    await service.update("gorevler", { alarm: true }, { id: id })
+    alert('Alarm güncellendi')
+    today();
+    tomorrow();
+  }
   return (
 
     <Box flex={1}>
@@ -112,8 +160,24 @@ function Home() {
         <HeaderInfo />
         <HeeaderReminder />
       </HeeaderContainer>
-      <DayTaskComponent />
-
+      <SectionList
+        sections={[
+          { title: 'Bugün', data: tasklisttoday },
+          { title: 'Yarın', data: tasklisttomorrow },
+        ]}
+        renderSectionHeader={({ section }) => (
+          <Text px={15} py={10} fontSize={18} fontWeight="bold" color="#8B87B3">{section.title}</Text>
+        )}
+        renderItem={({ item, index }) =>
+          <CardContainer index={index} mx={5} my={5} flexDirection='row' alignItems='center'>
+            <CardCheck onPress={() => completedtask(item.id)} bg={item.completed ? 'checkcontainercolor' : 'white'}  borderColor={item.completed ? 'white' : 'gray'} />
+            <CardDate px={5}>{item.time}</CardDate>
+            <CardTitle textdecoration={item.completed}>{item.title}</CardTitle>
+            <CardAlarm onPress={() => createalarm(item.id)} finish={item.alarm} />
+          </CardContainer>
+        }
+        keyExtractor={(item, index) => index}
+      />
     </Box>
   );
 }
