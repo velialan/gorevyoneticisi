f0c46e2b7e4bfcd14355936bca08c81f2809f427
velialan <veli.alan03@gmail.com>
2020-05-21 18:03:44 +0300

    düzenlemeler

--- src/Screens/Tasks.js
+++ src/Screens/Tasks.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Box from '../Components/box';
 import { HeeaderContainer, HeaderInfo } from '../Components/header';
-import {HeeaderReminder} from '../Components/headerreminder';
+import { HeeaderReminder } from '../Components/headerreminder';
 import TaskCard from '../Components/taskcard';
 function Tasks() {
   return (
     <Box flex={1}>
-        <HeeaderContainer p={10} >
+      <HeeaderContainer p={10} >
         <HeaderInfo />
         <HeeaderReminder />
       </HeeaderContainer>
-      <TaskCard/>
-
+      <TaskCard />
     </Box>
   );
 }
